b0dd9524cd9af298935838a7c78965ba8c038221
Danielle <danielle@zurb.com>
2012-07-17 15:21:56 -0700

    Initial commit with all Foundation pieces in place

---
+++ includes/_header.php
@@ -0,0 +1,94 @@
+<?
+  $version = "3.0";
+?>
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- For third-generation iPad with high-resolution Retina display: -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/favicons/apple-touch-icon-144x144-precomposed.png">
+  <!-- For iPhone with high-resolution Retina display: -->
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/favicons/apple-touch-icon-114x114-precomposed.png">
+  <!-- For first- and second-generation iPad: -->
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/favicons/apple-touch-icon-72x72-precomposed.png">
+  <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+  <link rel="apple-touch-icon-precomposed" href="images/favicons/apple-touch-icon-precomposed.png">
+  <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+
+  <link rel="icon" href="images/favicons/favicon.ico" type="image/x-icon" />
+
+  <meta name="keywords" content="foundation, design, frameworks, framework, css framework, html framework, html5, css3, SASS, SCSS, compass, responsive, design" />
+  <meta name="description" content="Foundation by ZURB is the most advanced responsive front-end framework in the world." />
+  <meta name="author" content="ZURB, inc. ZURB network also includes zurb.com" />
+  <meta name="copyright" content="ZURB, inc. Copyright (c) 2012" />
+
+	<title>Responsive Design <?= $page_title ?></title>
+
+	<!-- Included CSS Files -->
+  <link rel="stylesheet" href="http://www.zurb.com/assets/foundation.top-bar.css">
+  <link rel="stylesheet" href="http://www.zurb.com/assets/zurb.mega-drop.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/globals.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/typography.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/grid.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/ui.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/buttons.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/tabs.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/navbar.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/forms.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/orbit.css">
+	<link rel="stylesheet" href="stylesheets/foundation-style/reveal.css">
+	<link rel="stylesheet" href="stylesheets/app.css">
+
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+
+  <!-- ZURBar -->
+  <div class="container top-bar home-border">
+    <div class="attached">
+      <div class="name" onclick="void(0);">
+        <span><a href="http://zurb.com">ZURB</a> <a href="#" class="toggle-nav"></a></span>
+  		</div>
+
+  		<ul class="right">
+  			<li>
+  			  <a href="grid.php" <?php if (isset($featuresTab)) echo 'class="current"'; ?>>Home</a>
+  			</li>
+  			<li>
+  			  <a href="case-flite.php" <?php if (isset($caseStudiesTab)) echo 'class="current"'; ?>>Articles</a>
+  			</li>
+  			<li>
+  			  <a href="docs">Tools and Tips</a>
+  			</li>
+  		 <!-- <li>
+  		    <a href="http://github.com/zurb/foundation">GitHub</a>
+  			</li> -->
+  			<li class="download"><a class="small blue nice button src-download" href="download.php">Download Foundation</a></li>
+  		</ul>
+  	</div>
+  </div>
+
+  <!-- INSERT MEGA DROP DOWN HERE -->
+  <?php
+  $megadropfile = 'cache/navigation_bar.html';
+
+  if (file_exists($megadropfile)) {
+      include $megadropfile;
+  }
+  ?>
+
+  <!-- /ZURBar -->
---
+++ index.php
@@ -0,0 +1,160 @@
+<? $page_title = "The Most Advanced Responsive Front-end Framework from ZURB" ?>
+<?php include("includes/_header.php"); ?>
+
+  <header id="homepage">
+
+    <div class="row">
+      <div class="twelve columns">
+        <h1>How to Design Responsively</h1>
+        <h4>Tips, tricks, howtos and best practices from the team behind Foundation.</h4>
+      </div>
+    </div>
+
+  </header>
+
+  <section id="homepageFeatures">
+
+    <div class="row">
+      <div class="nine columns">
+
+        <div class="row">
+          <div class="four columns">
+            <a href="whats-new.php"><img src="http://placehold.it/140x100" /><!--<div class="pic" id="homepageImgSass"></div>--></a>
+
+            <h5><a href="#">Fluid Grid</a></h5>
+            <p>Foundation 3 is built with Sass, a powerful CSS preprocessor, which allows us to much more quickly develop Foundation itself &mdash; and gives you new tools to quickly customize and built on top of Foundation.</p>
+          </div>
+          <div class="four columns">
+            <a href="whats-new.php"><img src="http://placehold.it/140x100" /><!--<div class="pic" id="homepageImgFeatures"></div>--></a>
+
+            <h5><a href="#">Flexible Media</a></h5>
+            <p>We've added new elements, new options, and tons of streamlining to make this the fastest way to prototype responsively &mdash; and take that right into production.</p>
+          </div>
+          <div class="four columns">
+            <a href="whats-new.php"><img src="http://placehold.it/140x100" /><!--<div class="pic" id="homepageImgAdvanced"></div>--></a>
+
+            <h5><a href="#">Media Queries</a></h5>
+            <p>Foundation is the most advanced front-end framework in the world. While the framework works all the way back to IE8, we're taking advantage of new, awesome Web technology to help you build better and faster.</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="three columns">
+        <div id="github">
+          <h4>Get started with responsive design!</h4>
+          <a href="http://foundation.zurb.com" class="small button">Download Foundation</a>
+          <img src="http://placehold.it/140x100" />
+        </div>
+      </div>
+
+    </div>
+
+  </section>
+
+  <!--<section class="row" id="frameworkFeatures">
+    <div class="twelve columns">
+      <h3>Start here, build everywhere.</h3>
+
+      <div class="row">
+        <div class="four columns">
+          <a href="grid.php" class="pic" id="featureGrid"></a>
+          <h4><a href="grid.php">Flexible Grid</a></h4>
+
+          <p>The Grid lets you lay out pages quickly and logically with a flexible, nestable system. Even better, the Grid can be whatever size you need &mdash; it's easily adapted to any size screens, from phones to TVs.</p>
+
+        </div>
+        <div class="four columns">
+          <a href="prototyping.php" class="pic" id="featurePrototype"></a>
+          <h4><a href="prototyping.php">Rapid Prototyping</a></h4>
+
+          <p>Foundation includes dozens of styles and elements to help you quickly put together clickable prototypes, that can then be adapted and styled into polished production code. Forms, buttons, tabs, all kinds of good stuff.</p>
+
+        </div>
+        <div class="four columns">
+          <a href="mobile.php" class="pic" id="featureDevices"></a>
+          <h4><a href="mobile.php">Multidevice Mobility</a></h4>
+
+          <p>The Grid lets you quickly put together page layouts for mobile devices and the desktop. You don't need two different sites &mdash; the Grid is built to create a rock-solid experience on all kinds of devices with the exact same markup.</p>
+
+        </div>
+      </div>
+    </div>
+  </section>-->
+
+  <section id="caseStudies">
+   
+
+    <div class="row">
+      <div class="six columns">
+      	<h3><a href="#">Tools and Tips:</a></h3>
+        <div class="case-study">
+          <a href="case-flite.php"><img src="http://placehold.it/140x140" /></a>
+
+          <h4><a href="case-flite.php">Header</a></h4>
+          <p>When online advertising tech firm Flite needed to do a redesign of its website, it knew that it had to work on any device, not just desktops.<br /><a href="case-flite.php">Read more &rarr;</a></p>
+        </div>
+         <div class="case-study">
+          <a href="case-swizzle.php"><img src="http://placehold.it/140x140" /></a>
+
+          <h4><a href="case-swizzle.php">Navigation</a></h4>
+          <p>Online service and design agency Swizzle built this site just a month after the release of Foundation, and it's beautifully responsive.<br /><a href="case-swizzle.php">Read more &rarr;</a></p>
+        </div>
+      </div>
+      
+      <div class="six columns">
+      	<h3><a href="#">Popular Articles:</a></h3>
+       	<div class="case-study">
+          <a href="case-soapbox.php"><img src="http://placehold.it/140x140" /></a>
+
+          <h4><a href="case-soapbox.php">Forms</a></h4>
+          <p>One of the first sites built with Foundation, this site helps visitors to ZURB's speaking series learn, listen, and RSVP to events.<br /><a href="case-soapbox.php">Read more &rarr;</a></p>
+        </div>
+        <div class="case-study">
+          <a href="case-reel.php"><img src="http://placehold.it/140x140" /></a>
+
+          <h4><a href="case-reel.php">Sidebar</a></h4>
+          <p>A presentation hosting Web app built by ZURB, Foundation not only drives the entire app UI, but also drives its embeddable presentations.<br /><a href="case-reel.php">Read more &rarr;</a></p>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="row">
+    	<div class="six columns">
+    		<a href="#">See more tools and tips &rarr;</a>
+    	</div>
+    	<div class="six columns">
+    		<a href="#">See more articles &rarr;</a>
+    	</div>
+    </div>
+    
+   
+  </section>
+  
+  <hr />
+
+<!--  <section id="usingFoundation">
+    <div class="row">
+      <div class="twelve columns">
+        <h3>Sites Using Foundation</h3>
+        <a href="mailto:foundation@zurb.com" class="top-right">Is your site using Foundation? Let us know!</a>
+
+        <ul class="block-grid five-up">
+          <li><a href="http://projection.pixar.com/" id="usingPixar" target="_blank"><img src="images/usingPixar.png" /></a></li>
+          <li><a href="http://codesign.lanl.gov/" id="usingLosAlamos" target="_blank"><img src="images/usingLosAlamos.png" /></a></li>
+          <li><a href="http://www.flite.com/" id="usingFlite" target="_blank"><img src="images/usingFlite.png" /></a></li>
+          <li><a href="http://www.zurb.com" id="usingZURB" target="_blank"><img src="images/usingZURB.png" /></a></li>
+          <li><a href="http://www.getfelix.com/" id="usingFelix" target="_blank"><img src="images/usingFelix.png" /></a></li>
+          <li><a href="https://webfwd.org/" id="usingWebFWD" target="_blank"><img src="images/usingMozilla.png" /></a></li>
+          <li><a href="http://reelapp.com/" id="usingReel" target="_blank"><img src="images/usingReel.png" /></a></li>
+          <li><a href="http://spurapp.com/" id="usingSpur" target="_blank"><img src="images/usingSpur.png" /></a></li>
+          <li><a href="http://zurb.com/soapbox/" id="usingSoapbox" target="_blank"><img src="images/usingSoapbox.png" /></a></li>
+          <li><a href="http://axeapp.com/" id="usingAxe" target="_blank"><img src="images/usingAxe.png" /></a></li>
+        </ul>
+      </div>
+    </div>
+  </section> -->
+
+
+<?php include("includes/_footer.php");  ?>
