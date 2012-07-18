e2218551ca665636fd64ad73c1d2eade17a43a7b
Danielle <danielle@zurb.com>
2012-07-18 14:38:41 -0700

    created articles/individual article pages and styles

---
+++ articles.php
@@ -0,0 +1,195 @@
+<? $page_title = "Migrating from Foundation 2.2" ?>
+<?php include("includes/_header.php"); ?>
+
+  <header>
+    <div class="row">
+      <div class="twelve columns">
+        <h1>Articles</h1>
+        <h4>Looking to find out more about responsive design and how you should be using it?</h4>
+      </div>
+    </div>
+  </header>
+
+  <section id="mainContent">
+
+	<div class="row">
+		<div class="nine columns push-three">
+			
+			<div class="row">
+				<div class="twelve columns">
+					<h3>Featured:</h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="five columns">
+					<img src="http://placehold.it/270x270">
+				</div>
+				<div class="seven columns">
+					<h4>This is an example header for an article.</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+					<a href="individual.php" class="small button">Read More →</a>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="twelve columns">
+					<hr />
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+			</div>
+			
+			<div class="row">
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+			</div>
+			
+			<div class="row">
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+			</div>
+			
+			<div class="row">
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+				<div class="six columns">
+					<div class="row">
+						<div class="five columns">
+							<img src="http://placehold.it/150x150" class="bump-photo">
+						</div> <!-- five -->
+	
+						<div class="seven columns">
+							<h5>This is an example header for an article.</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								<a href="#">Read More →</a>
+							</p>
+							
+						</div> <!-- seven -->
+					</div> <!-- row -->
+				</div> <!-- six -->
+			</div>
+			
+		</div>
+		
+		<div class="three columns pull-nine">
+			<ul class="side-nav">
+			  <input class="input-search" id="search" type="text" placeholder="Search">
+			  <li class="divider"></li>
+			  <li>Popular Articles</li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <br />
+			  <li>Topics:</li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			
+			</ul>
+		</div>
+	</div>
+
+
+  </section>
+
+<?php include("includes/_footer.php");  ?>
---
+++ individual.php
@@ -0,0 +1,112 @@
+<? $page_title = "Migrating from Foundation 2.2" ?>
+<?php include("includes/_header.php"); ?>
+
+  <header>
+    <div class="row">
+      <div class="twelve columns">
+        <h1>Articles</h1>
+        <h4>Looking to find out more about responsive design and how you should be using it?</h4>
+      </div>
+    </div>
+  </header>
+
+  <section id="mainContent">
+
+	<div class="row">
+		<div class="nine columns push-three">
+			
+			<div class="row">
+				<h1>Article Title</h1>
+			</div>
+			
+			<div class="row">
+				<span class="date">January 1, 2012</span>
+				<span class="category">in</span>
+				<span class="topic">
+					<a class="post-category" href="http://www.zurb.com/blog/topic/zurb" title="ZURB"><strong>ZURB</strong></a>
+				</span>
+				<span class="category"> by Bryan</span>
+				<span>  with  </span>
+				<a href="#" class="post-counter">
+					<span class="post-comment-count">6 </span> comments
+				</a>
+				
+						<span class="tweet">
+							<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/tweet_button.1340179658.html#_=1342644544689&amp;count=horizontal&amp;id=twitter-widget-0&amp;lang=en&amp;original_referer=http%3A%2F%2Fwww.zurb.com%2Farticle%2F1030%2Fpersistence-pays-off-announcing-family-gi&amp;size=m&amp;text=Persistence%20Pays%20Off%3A%20Announcing%20Family%20Giving%20Tree%20as%20This%20Year's%20ZURBwired%20Nonprofit%20by%20ZURB&amp;url=http%3A%2F%2Fwww.zurb.com%2Farticle%2F1030%2Fpersistence-pays-off-announcing-family-gi&amp;via=ZURB" class="twitter-share-button twitter-count-horizontal" style="width: 107px; height: 20px; " title="Twitter Tweet Button"></iframe>
+						</span>
+				
+						<span class="fb"><iframe src="http://www.facebook.com/plugins/like.php?app_id=225656937481443&amp;href=http://www.zurb.com/article/1030/persistence-pays-off-announcing-family-gi&amp;send=false&amp;layout=button_count&amp;width=80&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" class="fb_iframe_widget" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:90px; height:21px;margin-bottom:8px;" allowtransparency="true"></iframe></span>
+						<span class="plusone">
+							<script type="text/javascript" src="https://apis.google.com/js/plusone.js" gapi_processed="true"></script>
+							<div id="___plusone_0" style="height: 20px; width: 90px; display: inline-block; text-indent: 0px; margin: 0px; padding: 0px; background-color: transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; background-position: initial initial; background-repeat: initial initial; "><iframe allowtransparency="true" frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 90px; margin: 0px; border-style: none; height: 20px; left: 0px; visibility: visible; " tabindex="0" vspace="0" width="100%" id="I0_1342644544594" name="I0_1342644544594" src="https://plusone.google.com/_/+1/fastbutton?bsv=pr&amp;url=http%3A%2F%2Fwww.zurb.com%2Farticle%2F1030%2Fpersistence-pays-off-announcing-family-gi&amp;size=medium&amp;count=true&amp;origin=http%3A%2F%2Fwww.zurb.com&amp;hl=en-US&amp;jsh=m%3B%2F_%2Fapps-static%2F_%2Fjs%2Fgapi%2F__features__%2Frt%3Dj%2Fver%3Dx-DNcn59B_E.en.%2Fsv%3D1%2Fam%3D!V1HSHW0XVB-YepGbpQ%2Fd%3D1%2Frs%3DAItRSTOlZCyvVd8kJgEW2dBJOL9l-VQ94Q#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart&amp;id=I0_1342644544594&amp;parent=http%3A%2F%2Fwww.zurb.com" title="+1"></iframe></div>
+						</span>
+						
+				
+					</ul>
+				</span>
+			</div>
+			
+			<div class="row">
+				<hr>
+			</div>
+			
+			<div class="row">
+				<div class="twelve columns">
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<img src="http://placehold.it/200x200" class="float-right"/>
+					<br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<br>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="two columns">
+				<p>This is a caption for this image. Still a caption. This is a caption for this image.</p>
+				</div>
+				<div class="ten columns">
+					<img src="http://placehold.it/500x150">
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="twelve columns">
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+			
+		</div>
+		
+		<div class="three columns pull-nine">
+			<ul class="side-nav">
+			  <input class="input-search" id="search" type="text" placeholder="Search">
+			   <li><a href="index.php">← Back to Articles</a></li>
+			  <li class="divider"></li>
+			  <li>Popular Articles</li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <br />
+			  <li>Topics:</li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			  <li><a href="#">Learn more about blah blah... </br>8 comments &rarr;</a></li>
+			
+			</ul>
+		</div>
+	</div>
+
+
+  </section>
+
+<?php include("includes/_footer.php");  ?>
