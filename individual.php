27e47e5d844dfafe7df0b32641e1c1e577686fe0
Danielle <danielle@zurb.com>
2012-07-20 15:28:55 -0700

    changes to individual.php

--- individual.php
+++ individual.php
@@ -4,9 +4,9 @@
   <header>
     <div class="row">
       <div class="twelve columns">
+		<a href="index.php" class="make-white"> &larr; Back to Articles</a>
 		<h1>Article Title</h1>
 		<h4>Looking to find out more about responsive design and how you should be using it?</h4>
-		<span class="make-white">January 1, 2012</span>
 		<a href="http://www.facebook.com/ZURB" target="_blank"><span class="glyph social make-white">d</span></a>
 		<a href="https://plusone.google.com/_/+1/confirm?url=http://foundation.zurb.com&amp;title=Foundation%20from%20ZURB" target="_blank"><span class="glyph social make-white">l</span></a>
 		<a href="http://www.twitter.com/foundationzurb" target="_blank"><span class="glyph social make-white">e</span></a>
@@ -17,34 +17,11 @@
   <section id="mainContent">
 
 	<div class="row">
-		<div class="nine columns">
+		<div class="nine columns">						
 			
 			<div class="row">
 				<div class="twelve columns">
-					<h4>Author's Name</h4>
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="four columns">
-					<img src="http://placehold.it/200x200" />
-				</div>
-				<div class="eight columns">
-					<h5>About:</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<a href="http://www.facebook.com/ZURB" target="_blank"><span class="glyph social author-social">d</span></a>
-					<a href="https://plusone.google.com/_/+1/confirm?url=http://foundation.zurb.com&amp;title=Foundation%20from%20ZURB" target="_blank"><span class="glyph social author-social">l</span></a>
-					<a href="http://www.twitter.com/foundationzurb" target="_blank"><span class="glyph social author-social">e</span></a>
-				</div>
-			</div>
-						
-			
-			<div class="row">
-				<hr>
-			</div>
-			
-			<div class="row">
-				<div class="twelve columns">
+					<p>January 1, 2012</p>
 					<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -75,9 +52,41 @@
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 					</p>
-					<a href="index.php">← Back to Articles</a>
 				</div>
 			</div>
+			
+			
+			<div class="row">
+				<hr>
+			</div>
+			
+			<div class="panel">
+				<div class="row">
+					<div class="twelve columns">
+						<h4>Author's Name</h4>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="four columns">
+						<img src="http://placehold.it/200x200" />
+					</div>
+					<div class="eight columns">
+						<h5>About:</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="http://www.facebook.com/ZURB" target="_blank"><span class="glyph social author-social">d</span></a>
+						<a href="https://plusone.google.com/_/+1/confirm?url=http://foundation.zurb.com&amp;title=Foundation%20from%20ZURB" target="_blank"><span class="glyph social author-social">l</span></a>
+						<a href="http://www.twitter.com/foundationzurb" target="_blank"><span class="glyph social author-social">e</span></a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="twelve columns">
+					<a href="index.php">&larr; Back to Articles</a>
+				</div>
+			</div>
+
 			
 		</div>
 		
